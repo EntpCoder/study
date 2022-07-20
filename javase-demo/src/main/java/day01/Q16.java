0bbb703541b2c3154e9f3493f6516c9b6aab2309
baicha1023 <1750929495@qq.com>
2022-07-20 16:11:41 +0800

    12542642412

--- javase-demo/src/main/java/day01/Q16.java
+++ javase-demo/src/main/java/day01/Q16.java
@@ -10,5 +10,10 @@ public class Q16 {
                 .filter(value -> Arrays.stream(arr).filter(v -> v == value).count() > 1)
                 .distinct()
                 .forEach(System.out::println);
+        Integer [] integer = new Integer[]{1,2,3,4,5,6,7,8,9,10};
+        fun1();
+    }
+    public static int fun1(int ...x){
+        return 0;
     }
 }
