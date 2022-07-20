2ba06d9367bda16a942fd432f492c1a3ea40dc44
Mr song <entpcoder@gmail.com>
2022-07-20 16:07:21 +0800

    "修改测试"

--- javase-demo/src/main/java/day03/TestClass.java
+++ javase-demo/src/main/java/day03/TestClass.java
@@ -4,5 +4,6 @@ public class TestClass {
     public static void main(String[] args) {
         System.out.println("bye world");
         System.out.println("");
+        System.out.println("1");
     }
 }
