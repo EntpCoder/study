c6fa30b591046352f227c184689cbc8d6f3ccc8e
Mr song <entpcoder@gmail.com>
2022-07-21 10:25:06 +0800

    "第三天作业"

--- javase-demo/src/main/java/day03/HomeWork.java
+++ javase-demo/src/main/java/day03/HomeWork.java
@@ -10,6 +10,8 @@ public class HomeWork {
      * 在整个过程中,牛牛会把所有在黑板上出现过的数字记录下来,然后求出他们的总和sum.
      *  例如X = 509, 在黑板上出现过的数字依次是509, 50, 5, 他们的和就是564.
      * 牛牛现在给出一个sum,牛牛想让你求出一个正整数X经过上述过程的结果是sum.
+     * @param sum 总和
+     * @return 正整数X
      */
     public static int getNum(int sum){
         for (int i = sum;i > 0;i--) {
@@ -24,6 +26,13 @@ public class HomeWork {
         }
         return -1;
     }
+
+    /**
+     * 把只包含质因子2、3和5的数称作丑数（Ugly Number）。
+     * 例如6、8都是丑数，但14不是，因为它包含质因子7。 习惯上我们把1当做是第一个丑数。求按从小到大的顺序的第N个丑数。
+     * @param n 第n个
+     * @return 丑数
+     */
     public static int getUglyNumbers(int n) {
         int[] arr = new int[n];
         arr[0] = 1;
